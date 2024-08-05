8f758e00d2b69d6cf80aac31a8db6cbcff9b6cd8
조윤근 <jo@joyungeun-ui-MacBookPro.local>
2024-08-05 12:55:16 +0900

    init

---
+++ TentenV10/TentenV10/TentenV10App.swift
@@ -0,0 +1,17 @@
+//
+//  TentenV10App.swift
+//  TentenV10
+//
+//  Created by 조윤근 on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TentenV10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
