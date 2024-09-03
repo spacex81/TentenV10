baa971be4e34b0a870c39f94225347f2fec213e6
조윤근 <jo@joyungeun-ui-MacBookPro.local>
2024-09-03 13:09:54 +0900

    Improve icon to svg

--- TentenV10/TentenV10/View/ContentView.swift
+++ TentenV10/TentenV10/View/ContentView.swift
@@ -1,18 +1,72 @@
+//import SwiftUI
+//import FirebaseAuth
+//
+//struct ContentView: View {
+//    @ObservedObject var viewModel = ContentViewModel()
+//    
+//    var body: some View {
+//        VStack {
+//            if viewModel.isUserLoggedIn {
+//                HomeView()
+//            } else {
+//                AuthView()
+//            }
+//        }
+//        .environmentObject(viewModel)
+//    }
+//}
+//
 import SwiftUI
 import FirebaseAuth
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ContentViewModel()
     
+    let iconSize = 20.0
     var body: some View {
-        VStack {
-            if viewModel.isUserLoggedIn {
-                HomeView()
-            } else {
-                AuthView()
+        VStack(alignment: .leading) {
+            HStack {
+                Image("apple")
+                    .resizable()
+                    .frame(width: iconSize, height: iconSize)
+                    .border(.red)
+                Text("Sign in with Apple")
+                    .font(.headline)
+                    .padding(.leading, 8)
+                    .border(.green)
             }
+            .padding(.vertical, 10)
+            .border(.black)
+            
+            HStack {
+                Image("google")
+                    .resizable()
+                    .frame(width: iconSize, height: iconSize)
+                    .border(.red)
+                Text("Sign in with Google")
+                    .font(.headline)
+                    .padding(.leading, 8)
+                    .border(.green)
+            }
+            .padding(.vertical, 10)
+            .border(.black)
+            
+            HStack {
+                Image("kakao")
+                    .resizable()
+                    .frame(width: iconSize, height: iconSize)
+                    .border(.red)
+                Text("Sign in with Kakao")
+                    .font(.headline)
+                    .padding(.leading, 8)
+                    .border(.green)
+            }
+            .padding(.vertical, 10)
+            .border(.black)
         }
-        .environmentObject(viewModel)
+        .padding()
     }
 }
- 
+
+#Preview {
+    ContentView()
+}
