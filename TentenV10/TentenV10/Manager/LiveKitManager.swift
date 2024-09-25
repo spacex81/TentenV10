7af64725a9b86dbf9388338673cefaed19b31faf
조윤근 <jo@joyungeun-ui-MacBookPro.local>
2024-09-25 17:53:56 +0900

    Move sendLocalNotification func from LiveKitManager to RepositoryManager

--- TentenV10/TentenV10/Manager/LiveKitManager.swift
+++ TentenV10/TentenV10/Manager/LiveKitManager.swift
@@ -220,7 +220,8 @@ extension LiveKitManager {
     func room(_ room: Room, participant: RemoteParticipant, didSubscribeTrack publication: RemoteTrackPublication) {
         sendMessageToRemoteParticipant(message: "readyToTalk")
         self.repoManager?.currentState = .isListening
-        sendLocalNotification(title: "Remote Participant", body: "Remote participant is talking")
+//        sendLocalNotification(title: "Remote Participant", body: "Remote participant is talking")
+        self.repoManager?.sendLocalNotification(title: "Remote Participant", body: "Remote participant is talking")
         NSLog("LOG: Ready to listen to remote audio stream")
     }
     
@@ -256,18 +257,18 @@ extension LiveKitManager {
         }
     }
     
-    func sendLocalNotification(title: String, body: String) {
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.body = body
-        content.sound = .default
-        
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
-        
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("Failed to add notification request: \(error)")
-            }
-        }
-    }
+//    func sendLocalNotification(title: String, body: String) {
+//        let content = UNMutableNotificationContent()
+//        content.title = title
+//        content.body = body
+//        content.sound = .default
+//        
+//        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
+//        
+//        UNUserNotificationCenter.current().add(request) { error in
+//            if let error = error {
+//                print("Failed to add notification request: \(error)")
+//            }
+//        }
+//    }
 }
--- TentenV10/TentenV10/Manager/RepositoryManager.swift
+++ TentenV10/TentenV10/Manager/RepositoryManager.swift
@@ -1146,6 +1146,23 @@ extension RepositoryManager {
     //  add two function that increment/decrement isActive value by 1
 }
 
+extension RepositoryManager {
+    func sendLocalNotification(title: String, body: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Failed to add notification request: \(error)")
+            }
+        }
+    }
+}
+
 enum UserState {
     case isSpeaking
     case isListening
