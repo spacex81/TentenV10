bb86393cc131e4fdd82fb251653efbb375c1e07b
조윤근 <jo@joyungeun-ui-MacBookPro.local>
2024-10-02 14:41:29 +0900

    Change DeleteView's appearance method from sheet to overlay

--- TentenV10/TentenV10/View/Home/FriendCenter/ProfileView.swift
+++ TentenV10/TentenV10/View/Home/FriendCenter/ProfileView.swift
@@ -1,196 +1,210 @@
 import SwiftUI
 
 struct ProfileView: View {
-//    @State private var isSheetPresented: Bool = false
     @State private var isAddFriendSheetPresented: Bool = false
-    @State private var isDeleteFriendSheetPresented: Bool = false
     @State private var friendToDelete: FriendRecord?
     
-//   @ObservedObject var viewModel = HomeViewModel.shared // Case 1: Use on real app
-    let viewModel = HomeViewModel.shared // Case 2: Only use when building with preview
+    // State for showing delete view as an overlay
+    @State private var showDeleteOverlay = false
     
-    private let impactFeedback = UIImpactFeedbackGenerator(style: .medium)
+    let viewModel = HomeViewModel.shared
 
     var body: some View {
-        VStack {
-            if let userRecord = viewModel.userRecord, let imageData = userRecord.profileImageData, let uiImage = UIImage(data: imageData) {
-                
-                // user profile panel
-                HStack {
-                    // username and pin
-                    VStack(alignment: .leading) {
-                        Text(userRecord.username)
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .padding(.bottom, 1)
-                        if let userPin = viewModel.userRecord?.pin {
-                            PinButton(pin: userPin)
-                        }
-                    }
-                    Spacer()
-                    // profile pic
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-                        .shadow(radius: 10)
-                }
-                .padding(10)
-                
-                // add friends button
-                Button {
-                    isAddFriendSheetPresented = true
-                    impactFeedback.impactOccurred()
-                } label: {
+        ZStack {
+            VStack {
+                if let userRecord = viewModel.userRecord, let imageData = userRecord.profileImageData, let uiImage = UIImage(data: imageData) {
+
+                    // User profile panel
                     HStack {
-                        Image(systemName: "plus")
-                            .tint(.white)
-                            .fontWeight(.bold)
-                            .font(.title2)
-                        Text("add friends")
-                            .tint(.white)
-                            .fontWeight(.bold)
-                            .font(.title2)
-                        Spacer()
-                    }
-                    .padding(25)
-                    .background(Color(UIColor(white: 0.3, alpha: 1.0)))
-                    .cornerRadius(20)
-                    .frame(maxWidth: .infinity)
-                }
-                .frame(maxWidth: .infinity)
-                
-                // Friends List with Three-dot Button
-                ScrollView {
-                    VStack {
-                        ForEach(viewModel.detailedFriends, id: \.id) { friend in
-                            HStack {
-                                // Friend's profile pic and name
-                                HStack {
-                                    if let friendImageData = friend.profileImageData, let friendImage = UIImage(data: friendImageData) {
-                                        Image(uiImage: friendImage)
-                                            .resizable()
-                                            .scaledToFill()
-                                            .frame(width: 50, height: 50)
-                                            .clipShape(Circle())
-                                            .shadow(radius: 5)
-                                    }
-                                    Text(friend.username)
-                                        .font(.headline)
-                                        .padding(.leading, 10)
-                                }
-                                Spacer()
-                                
-                                // Three-dot menu button
-                                Button(action: {
-                                    friendToDelete = friend
-                                }) {
-                                    Image(systemName: "ellipsis")
-                                        .foregroundColor(Color.gray)
-                                        .padding(.trailing, 10)
-                                }
+                        VStack(alignment: .leading) {
+                            Text(userRecord.username)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .padding(.bottom, 1)
+                            if let userPin = viewModel.userRecord?.pin {
+                                PinButton(pin: userPin)
                             }
-                            .padding()
+                        }
+                        Spacer()
+                        Image(uiImage: uiImage)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                    }
+                    .padding(10)
+
+                    // Add friends button
+                    Button {
+                        isAddFriendSheetPresented = true
+                    } label: {
+                        HStack {
+                            Image(systemName: "plus")
+                                .tint(.white)
+                                .fontWeight(.bold)
+                                .font(.title2)
+                            Text("add friends")
+                                .tint(.white)
+                                .fontWeight(.bold)
+                                .font(.title2)
+                            Spacer()
+                        }
+                        .padding(25)
+                        .background(Color(UIColor(white: 0.3, alpha: 1.0)))
+                        .cornerRadius(20)
+                        .frame(maxWidth: .infinity)
+                    }
+
+                    // Friends List with Three-dot Button
+                    ScrollView {
+                        VStack {
+                            ForEach(viewModel.detailedFriends, id: \.id) { friend in
+                                HStack {
+                                    // Friend's profile pic and name
+                                    HStack {
+                                        if let friendImageData = friend.profileImageData, let friendImage = UIImage(data: friendImageData) {
+                                            Image(uiImage: friendImage)
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: 50, height: 50)
+                                                .clipShape(Circle())
+                                                .shadow(radius: 5)
+                                        }
+                                        Text(friend.username)
+                                            .font(.headline)
+                                            .padding(.leading, 10)
+                                    }
+                                    Spacer()
+
+                                    // Three-dot menu button
+                                    Button(action: {
+                                        withAnimation {
+                                            friendToDelete = friend
+                                            showDeleteOverlay = true
+                                        }
+                                    }) {
+                                        Image(systemName: "ellipsis")
+                                            .foregroundColor(Color.gray)
+                                            .padding(.trailing, 10)
+                                    }
+                                }
+                                .padding()
+                            }
                         }
                     }
+                    .background(Color.clear)
                 }
-                .background(Color.clear)
+            }
+            .padding(30)
+            .sheet(isPresented: $isAddFriendSheetPresented) {
+                AddFriendView()
+            }
+            
+            // Overlay for DeleteView with animation
+            if showDeleteOverlay {
+                Color.black.opacity(0.5)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation {
+                            showDeleteOverlay = false  // Dismiss overlay with animation
+                        }
+                    }
+
+                // Bottom to top sliding DeleteView
+                GeometryReader { geometry in
+                    VStack {
+                        Spacer()
+                        DeleteView(friend: $friendToDelete)
+                            .frame(width: geometry.size.width, height: geometry.size.height * 0.4)  // Full width, 40% height
+                            .background(Color.white)
+                            .cornerRadius(20)
+                            .shadow(radius: 10)
+                            .transition(.move(edge: .bottom))  // Transition animation from bottom to top
+                    }
+                }
+                .zIndex(1)  // Ensure the DeleteView appears above everything else
             }
         }
-        .padding(30)
-        .sheet(isPresented: $isAddFriendSheetPresented) {
-            AddFriendView()
-        }
-        .onChange(of: friendToDelete) { _, _ in
-            if friendToDelete != nil {
-                isDeleteFriendSheetPresented = true
-            }
-        }
-        .sheet(isPresented: $isDeleteFriendSheetPresented) {
-            DeleteView(friend: $friendToDelete)
-                .presentationDetents([.fraction(0.4)])  // Set sheet to half height
-        }
-        .frame(maxWidth: .infinity)
     }
 }
 
-var dummyUserRecord: UserRecord {
-    let image = UIImage(named: "user1")!  // Use system image for preview
-    let imageData = image.pngData()
-    return UserRecord(
-        id: UUID().uuidString,
-        email: "john.doe@example.com",
-        username: "John Doe",
-        password: "dummyPassword123",
-        pin: "123456",
-        hasIncomingCallRequest: false,
-        profileImageData: imageData,
-        deviceToken: nil,
-        friends: ["friend1", "friend2"],
-        roomName: "testRoom",
-        isBusy: false,
-        socialLoginId: "dummySocialLoginId",
-        socialLoginType: "facebook",
-        imageOffset: 0.0
-    )
-}
-
-let dummyFriend = FriendRecord(
-    id: UUID().uuidString,
-    email: "friend1@example.com",
-    username: "Dummy Friend",
-    pin: "111111",
-    profileImageData: UIImage(named: "user2")?.pngData(),
-    deviceToken: nil,
-    userId: "userId1",
-    isBusy: false,
-    lastInteraction: Date()
-)
-
-var dummyFriends: [FriendRecord] {
-    return [
-        FriendRecord(
-            id: UUID().uuidString,
-            email: "friend1@example.com",
-            username: "Friend 1",
-            pin: "111111",
-            profileImageData: UIImage(named: "user2")?.pngData(), // Image from Assets
-            deviceToken: nil,
-            userId: "userId1",
-            isBusy: false,
-            lastInteraction: Date()
-        ),
-        FriendRecord(
-            id: UUID().uuidString,
-            email: "friend2@example.com",
-            username: "Friend 2",
-            pin: "222222",
-            profileImageData: UIImage(named: "user3")?.pngData(), // Image from Assets
-            deviceToken: nil,
-            userId: "userId2",
-            isBusy: false,
-            lastInteraction: Date()
-        ),
-        // Add more friends with images from "user4" to "user10"
-        FriendRecord(
-            id: UUID().uuidString,
-            email: "friend3@example.com",
-            username: "Friend 3",
-            pin: "333333",
-            profileImageData: UIImage(named: "user4")?.pngData(),
-            deviceToken: nil,
-            userId: "userId3",
-            isBusy: false,
-            lastInteraction: Date()
-        )
-    ]
-}
-
 #Preview {
+    
+    var dummyUserRecord: UserRecord {
+        let image = UIImage(named: "user1")!  // Use system image for preview
+        let imageData = image.pngData()
+        return UserRecord(
+            id: UUID().uuidString,
+            email: "john.doe@example.com",
+            username: "John Doe",
+            password: "dummyPassword123",
+            pin: "123456",
+            hasIncomingCallRequest: false,
+            profileImageData: imageData,
+            deviceToken: nil,
+            friends: ["friend1", "friend2"],
+            roomName: "testRoom",
+            isBusy: false,
+            socialLoginId: "dummySocialLoginId",
+            socialLoginType: "facebook",
+            imageOffset: 0.0
+        )
+    }
+
+    let dummyFriend = FriendRecord(
+        id: UUID().uuidString,
+        email: "friend1@example.com",
+        username: "Dummy Friend",
+        pin: "111111",
+        profileImageData: UIImage(named: "user2")?.pngData(),
+        deviceToken: nil,
+        userId: "userId1",
+        isBusy: false,
+        lastInteraction: Date()
+    )
+
+    var dummyFriends: [FriendRecord] {
+        return [
+            FriendRecord(
+                id: UUID().uuidString,
+                email: "friend1@example.com",
+                username: "Friend 1",
+                pin: "111111",
+                profileImageData: UIImage(named: "user2")?.pngData(), // Image from Assets
+                deviceToken: nil,
+                userId: "userId1",
+                isBusy: false,
+                lastInteraction: Date()
+            ),
+            FriendRecord(
+                id: UUID().uuidString,
+                email: "friend2@example.com",
+                username: "Friend 2",
+                pin: "222222",
+                profileImageData: UIImage(named: "user3")?.pngData(), // Image from Assets
+                deviceToken: nil,
+                userId: "userId2",
+                isBusy: false,
+                lastInteraction: Date()
+            ),
+            FriendRecord(
+                id: UUID().uuidString,
+                email: "friend3@example.com",
+                username: "Friend 3",
+                pin: "333333",
+                profileImageData: UIImage(named: "user4")?.pngData(),
+                deviceToken: nil,
+                userId: "userId3",
+                isBusy: false,
+                lastInteraction: Date()
+            )
+        ]
+    }
+    
     HomeViewModel.shared.userRecord = dummyUserRecord
     HomeViewModel.shared.detailedFriends = dummyFriends
-    
+
     return ProfileView()
         .preferredColorScheme(.dark)
 }
