478153944dd032c34b0d07e65e5c3c9e4115bb88
조윤근 <jo@joyungeun-ui-MacBookPro.local>
2024-10-02 10:22:51 +0900

    Add DeleteFriendCard

---
+++ TentenV10/TentenV10/View/Home/FriendCenter/DeleteFriendCard.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteFriendCard.swift
+//  TentenV10
+//
+//  Created by 조윤근 on 10/2/24.
+//
+
+import SwiftUI
+
+struct DeleteFriendCard: View {
+    var body: some View {
+        Text("Delete Friend Card")
+    }
+}
+
+#Preview {
+    DeleteFriendCard()
+}
